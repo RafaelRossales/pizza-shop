81e70952f723e2be4cb033d5ea9729c686bc6076
Rafael_Rossales <rapha2704@gmail.com>
2024-09-15 18:48:34 -0300

    Implementing orders pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import SignIn from "./pages/auth/signin";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import SignUp from "./pages/auth/signup";
+import Orders from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
 
@@ -11,7 +12,8 @@ export const router = createBrowserRouter([
         path:'/', 
         element:<AppLayout/>,
         children:[
-            {path:'/', element:<Dashboard/>}
+            {path:'/', element:<Dashboard/>},
+            {path:'/orders', element:<Orders/>}
         ]
     },
     {
